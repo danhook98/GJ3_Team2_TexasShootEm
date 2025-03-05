d467f5f285d1123faff82be277b74fe38904aec0
Dan Hook <s188873@uos.ac.uk>
2025-03-05 17:32:46 +0000

    Convert accuracy slider input to use InputReader

--- Assets/_Scripts/AccuracySlider.cs
+++ Assets/_Scripts/AccuracySlider.cs
@@ -1,46 +1,60 @@
-using UnityEngine;
-using UnityEngine.Serialization;
-using UnityEngine.UI;
+using System;
 using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
-public class AccuracySlider : MonoBehaviour
+namespace TexasShootEm
 {
-    [SerializeField] private Slider accuracySlider;
-    [SerializeField] private AnimationCurve lerpCurve;
-    
-    [SerializeField] private float _difficultyMultiplier = 0.25f;
-    
-    private float _accuracyScore;
-    private float _accuracySliderValue;
-    private float _valueChange; 
-    private float _sliderSpeed;
-    private bool _isSliderPaused;
-    
-    private Dictionary<string, float> scoreResults = new Dictionary<string, float>();
-    private float _perfectScoreRange = 0.9f;
-    private float _goodScoreRange = 0.6f;
-    private float _okayScoreRange = 0.3f;
-    private float _badScoreRange;
-    private float _missScoreRange;
-    
-    private void Start()
+    public class AccuracySlider : MonoBehaviour
     {
-        _isSliderPaused = false;
-        AddNewScoreResults();
-    }
+        [SerializeField] private InputReader inputReader; 
+        [SerializeField] private Slider accuracySlider;
+        [SerializeField] private AnimationCurve lerpCurve;
     
-    private void Update()
-    {
-        if (!_isSliderPaused)
+        [SerializeField] private float difficultyMultiplier = 0.25f;
+    
+        private float _accuracyScore;
+        private float _accuracySliderValue;
+        private float _valueChange; 
+        private float _sliderSpeed;
+        private bool _isSliderPaused;
+    
+        private Dictionary<string, float> scoreResults;
+        private float _perfectScoreRange = 0.9f;
+        private float _goodScoreRange = 0.6f;
+        private float _okayScoreRange = 0.3f;
+        private float _badScoreRange;
+        private float _missScoreRange;
+
+        private void Awake()
         {
-            CalculateValueChange();
+            scoreResults = new Dictionary<string, float>();
+            _isSliderPaused = false;
+            AddNewScoreResults();
         }
-        
-        // For scoring, score ranges from 0 to 1, closer to the middle of the bar closer to a value of 1.
-        if (Input.GetKeyDown(KeyCode.Space))
+
+        private void OnEnable() => inputReader.OnAimEvent += GetSliderValue;
+        private void OnDisable() => inputReader.OnAimEvent -= GetSliderValue;
+
+        private void Update()
         {
+            if (!_isSliderPaused)
+            {
+                CalculateValueChange();
+            }
+
+            // For Testing
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+                ResetAccuracySlider();
+            }
+        }
+
+        private void GetSliderValue()
+        {
+            // For scoring, score ranges from 0 to 1, closer to the middle of the bar closer to a value of 1.
             _isSliderPaused = true;
-            
+        
             // Calculate score when slider value is above or below the halfway mark.
             if (accuracySlider.value > 0.5f)
             {
@@ -54,41 +68,28 @@ public class AccuracySlider : MonoBehaviour
                 _accuracySliderValue = accuracySlider.value;
                 _accuracySliderValue = 2 * _accuracySliderValue;
             }
-            
+        
             _accuracyScore = _accuracySliderValue;
             Debug.Log(_accuracyScore);
         }
 
-        // For Testing
-        if (Input.GetKeyDown(KeyCode.T))
+        private void CalculateValueChange()
         {
-            ResetAccuracySlider();
+            float pingPongValue = Mathf.PingPong(Time.time * difficultyMultiplier, accuracySlider.maxValue);
+            _valueChange = lerpCurve.Evaluate(pingPongValue);
+            accuracySlider.value = _valueChange;
         }
-    }
-    
-    public void SetDifficulty(float newMultiplier)
-    {
-        _difficultyMultiplier = newMultiplier;
-    }
 
-    private void CalculateValueChange()
-    {
-        float pingPongValue = Mathf.PingPong(Time.time * _difficultyMultiplier, accuracySlider.maxValue);
-        _valueChange = lerpCurve.Evaluate(pingPongValue);
-        accuracySlider.value = _valueChange;
-    }
-
-    public void ResetAccuracySlider()
-    {
-        _isSliderPaused = false;
-    }
-
-    private void AddNewScoreResults()
-    {
-        scoreResults.Add("Perfect!", _perfectScoreRange);
-        scoreResults.Add("Good!", _goodScoreRange);
-        scoreResults.Add("Okay!", _okayScoreRange);
-        scoreResults.Add("Bad!", _badScoreRange);
-        scoreResults.Add("Miss!", _missScoreRange);
+        private void AddNewScoreResults()
+        {
+            scoreResults.Add("Perfect!", _perfectScoreRange);
+            scoreResults.Add("Good!", _goodScoreRange);
+            scoreResults.Add("Okay!", _okayScoreRange);
+            scoreResults.Add("Bad!", _badScoreRange);
+            scoreResults.Add("Miss!", _missScoreRange);
+        }
+        
+        public void SetDifficulty(float newMultiplier) => difficultyMultiplier = newMultiplier;
+        public void ResetAccuracySlider() => _isSliderPaused = false;
     }
 }
