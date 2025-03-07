0203d1938f909ad0f9c59ab9282b4c41b3be5809
NikolaosComandariu <s283743@uos.ac.uk>
2025-03-07 11:43:20 +0000

    Finished timer script

--- Assets/_Scripts/UI/Timer.cs
+++ Assets/_Scripts/UI/Timer.cs
@@ -1,36 +1,87 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TexasShootEm.EventSystem;
+using TMPro;
 
 namespace TexasShootEm
 {
     public class Timer : MonoBehaviour
     {
-        // Start is called before the first frame update
+        [SerializeField] private VoidEvent onGameOverEvent;
+
+        private float _timer = 30f; // TODO: Set this to 0 after testing.
+        private TextMeshProUGUI _timerText;
+        private bool _isCountingDown;
+        
         void Start()
         {
-        
+            _timerText = GetComponent<TextMeshProUGUI>();
         }
-
-        // Update is called once per frame
+        
         void Update()
         {
+            // Display timer
+            _timerText.text = _timer.ToString("00");
+            if (_isCountingDown)
+            {
+                _timer -= Time.deltaTime;
+            }
+            
             // IF timer reaches 0, invoke GameOver game event.
+            if (_timer <= 0)
+            {
+                onGameOverEvent.Invoke(new Empty());
+            }
+            
+            // TODO: Remove these, they only exist for testing purposes.
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Debug.Log("Starting Timer");
+                StartTimer();
+            }
+
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                Debug.Log("Pausing Timer");
+                PauseTimer();
+            }
+
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+                Debug.Log("Increasing Timer by 2s");
+                ModifyTimer(2f);
+            }
+
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                Debug.Log("Decreasing Timer by 2s");
+                ModifyTimer(-2f);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Y))
+            {
+                Debug.Log("Setting timer to 20s");
+                SetTimeLimit(20f);
+            }
         }
 
         public void SetTimeLimit(float time)
         {
-            
+            _timer = time;
         }
 
         public void StartTimer()
         {
-            
+            _isCountingDown = true;
+        }
+
+        public void PauseTimer()
+        {
+            _isCountingDown = false;
         }
 
         public void ModifyTimer(float time)
         {
-            
+            _timer += time;
         }
     }
 }
