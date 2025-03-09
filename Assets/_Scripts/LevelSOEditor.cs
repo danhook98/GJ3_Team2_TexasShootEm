85b948a0b66e87a26e54994e343f644c78dd1e24
Dan Hook <s188873@uos.ac.uk>
2025-03-09 12:20:02 +0000

    Use LevelSO custom editor to organise properties

--- Assets/_Scripts/LevelSOEditor.cs
+++ Assets/_Scripts/LevelSOEditor.cs
@@ -6,15 +6,23 @@ namespace TexasShootEm
 {
     [CustomEditor(typeof(LevelSO))]
     public class LevelSOEditor : Editor
-    {        
+    {
+        private SerializedProperty _levelNumber;
+        private SerializedProperty _levelUnlocked;
         private SerializedProperty _difficultyProperty;
+        private SerializedProperty _accuracySliderData;
+        private SerializedProperty _keyPresses;
+        
         private string[] difficultyList = { "Beginner", "Intermediate", "Expert" };
         private int selectedIndex = 0;
         
         private void OnEnable()
         {
-            // Find the difficulty property from the LevelSO data.
+            _levelNumber = serializedObject.FindProperty("LevelNumber");
+            _levelUnlocked = serializedObject.FindProperty("Unlocked");
             _difficultyProperty = serializedObject.FindProperty("Difficulty");
+            _accuracySliderData = serializedObject.FindProperty("AccuracySliderData");
+            _keyPresses = serializedObject.FindProperty("KeyPresses");
             
             // Ensure that the index of the dropdown is the same as what's saved in the LevelSO data.
             selectedIndex = Array.IndexOf(difficultyList, _difficultyProperty.stringValue);
@@ -24,11 +32,13 @@ namespace TexasShootEm
 
         public override void OnInspectorGUI()
         {
-            
             // Display the base inspector GUI.
-            base.OnInspectorGUI();
+            //base.OnInspectorGUI();
             
             serializedObject.Update();
+            
+            EditorGUILayout.PropertyField(_levelNumber);
+            EditorGUILayout.PropertyField(_levelUnlocked);
             
             // Display the dropdown of difficulties.
             selectedIndex = EditorGUILayout.Popup("Difficulty", selectedIndex, difficultyList);
@@ -36,6 +46,9 @@ namespace TexasShootEm
             // Update the difficulty property string text with what was selected.
             _difficultyProperty.stringValue = difficultyList[selectedIndex];
 
+            EditorGUILayout.PropertyField(_accuracySliderData);
+            EditorGUILayout.PropertyField(_keyPresses);
+            
             serializedObject.ApplyModifiedProperties();
         }
     }
