caafabbce1462e8ae684ff1fa3cf1542dfe469cb
NikolaosComandariu <s283743@uos.ac.uk>
2025-03-09 15:02:47 +0000

    Add changes based on feedback

--- Assets/_Scripts/EntityController.cs
+++ Assets/_Scripts/EntityController.cs
@@ -10,10 +10,11 @@ namespace TexasShootEm
         [SerializeField] private VoidEvent entityShoot;
         [SerializeField] private VoidEvent entityDeath;
         [SerializeField] private VoidEvent entityAim;
+        [SerializeField] private AudioClipSOEvent playSfx;
 
         [Header("Audio Clip SO")]
-        [SerializeField] private AudioClipSO entityDeathSFX;
-        [SerializeField] private AudioClipSO entityShootSFX;
+        [SerializeField] private AudioClipSO entityDeathSfx;
+        [SerializeField] private AudioClipSO entityShootSfx;
 
         private Animator _entityAnim;
         private bool _isDead = false;
@@ -48,6 +49,7 @@ namespace TexasShootEm
                 Debug.Log("Starting shoot coroutine");
                 _entityAnim.SetTrigger("Shoot");
                 yield return new WaitForSeconds(0.2f);
+                playSfx.Invoke(entityShootSfx);
                 entityShoot.Invoke(new Empty());
                 yield return new WaitForSeconds(0.5f);
             }
@@ -59,6 +61,7 @@ namespace TexasShootEm
             Debug.Log("Starting death coroutine");
             _entityAnim.SetTrigger("Death");
             entityDeath.Invoke(new Empty());
+            playSfx.Invoke(entityDeathSfx);
             yield return new WaitForSeconds(0.9f);
             _entityAnim.SetTrigger("StayDead");
         }
