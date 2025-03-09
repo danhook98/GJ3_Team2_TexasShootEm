d70f30e013573ceca6d6f020f2c15d320acfd11c
Dan Hook <s188873@uos.ac.uk>
2025-03-09 14:33:21 +0000

    Remove button click events from level select, prepare play button
    The button audio click will be replaced.

--- Assets/_Scripts/LevelDataLoader.cs
+++ Assets/_Scripts/LevelDataLoader.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 namespace TexasShootEm
@@ -10,6 +11,10 @@ namespace TexasShootEm
     public class LevelDataLoader : MonoBehaviour
     {
         [SerializeField] private LevelSO levelSO;
+        [SerializeField] private LevelLoadSO levelLoadSO;
+        [Space] 
+        [SerializeField] private AudioClipSO clickSound;
+        
         [SerializeField] private TextMeshProUGUI titleText;
         [SerializeField] private TextMeshProUGUI difficultyText;
         [SerializeField] private Button playButton;
@@ -27,6 +32,13 @@ namespace TexasShootEm
         {
             playButton.interactable = levelSO.Unlocked;
             buttonText.text = levelSO.Unlocked ? "Play" : "Locked";
+        }
+
+        public void Play()
+        {
+            levelLoadSO.loadedLevel = levelSO;
+            Debug.Log($"Loading level {levelSO.LevelNumber}");
+            SceneManager.LoadScene("MainGame");
         }
     }
 }
