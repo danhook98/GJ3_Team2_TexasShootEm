d08c39fff3751b82c777bced3cbcfdbfbb86e37b
NikolaosComandariu <s283743@uos.ac.uk>
2025-03-09 11:01:30 +0000

    Add Player coroutines

--- Assets/_Scripts/EnemyController.cs
+++ Assets/_Scripts/EnemyController.cs
@@ -8,56 +8,91 @@ namespace TexasShootEm
     public class EnemyController : MonoBehaviour
     {
         [Header("Events")] 
-        [SerializeField] private VoidEvent shoot;
-        [SerializeField] private VoidEvent death;
+        [SerializeField] private VoidEvent evilShoot;
+        [SerializeField] private VoidEvent evilDeath;
+        [SerializeField] private VoidEvent sheriffShoot;
+        [SerializeField] private VoidEvent sheriffDeath;
+        [SerializeField] private VoidEvent sheriffAim;
 
         [Header("Animators")] 
         [SerializeField] private Animator sheriffAnim;
         [SerializeField] private Animator evilAnim;
         
-        //private Animator evilTubboAnim;
         private bool _isDead = false;
-
-        private void Start()
-        {
-            evilAnim = GetComponent<Animator>();
-        }
 
         private void Update() // TODO: Remove this, this is here only for testing purposes.
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                StartCoroutine(Shoot());
+                StartCoroutine(EvilTubboShoot());
+                StartCoroutine(SheriffShoot());
             }
 
             if (Input.GetKeyDown(KeyCode.T))
             {
-                StartCoroutine(Death());
+                StartCoroutine(EvilTubboDeath());
+                StartCoroutine(SheriffDeath());
             }
-        }
 
-        public IEnumerator Shoot()
-        {
-            if (!_isDead)
+            if (Input.GetKeyDown(KeyCode.R))
             {
-                Debug.Log("Starting shoot coroutine");
-                evilAnim.SetTrigger("Shoot");
-                yield return new WaitForSeconds(0.5f);
-                shoot.Invoke(new Empty());
-                yield return new WaitForSeconds(0.5f);
+                StartCoroutine(SheriffAim());
             }
         }
 
-        public IEnumerator Death()
+        public IEnumerator EvilTubboShoot()
+        {
+            if (_isDead) yield return null;
+            
+            Debug.Log("Starting enemy shoot coroutine");
+            evilAnim.SetTrigger("Shoot");
+            yield return new WaitForSeconds(0.5f);
+            evilShoot.Invoke(new Empty());
+            yield return new WaitForSeconds(0.5f);
+        }
+
+        public IEnumerator EvilTubboDeath()
         {
             _isDead = true;
-            Debug.Log("Starting death coroutine");
+            Debug.Log("Starting enemy death coroutine");
             evilAnim.SetTrigger("Death");
-            death.Invoke(new Empty());
+            evilDeath.Invoke(new Empty());
             yield return new WaitForSeconds(0.7f);
             evilAnim.SetTrigger("StayDead");
+        }
+
+        public IEnumerator SheriffShoot()
+        {
+            if (_isDead) yield return null;
+            
+            Debug.Log("Starting player shoot coroutine");
+            sheriffAnim.SetTrigger("Shoot");
+            yield return new WaitForSeconds(0.5f);
+            sheriffShoot.Invoke(new Empty());
+            yield return new WaitForSeconds(0.5f);
+        }
+        
+        public IEnumerator SheriffDeath()
+        {
+            _isDead = true;
+            Debug.Log("Starting player death coroutine");
+            sheriffAnim.SetTrigger("Death");
+            sheriffDeath.Invoke(new Empty());
+            yield return new WaitForSeconds(0.7f);
+            sheriffAnim.SetTrigger("StayDead");
+        }
+
+        public IEnumerator SheriffAim()
+        {
+            if (_isDead) yield return null;
+            
+            Debug.Log("Starting player aim coroutine");
+            sheriffAnim.SetTrigger("Aim");
+            sheriffAim.Invoke(new Empty());
+            yield return new WaitForSeconds(0.5f);
+            sheriffAnim.SetTrigger("StayAiming");
         }
     }
 }
 
-// Trigger names for Sheriff Tubbo: Aim, Shoot, Death, StayDead
+// Trigger names for Sheriff Tubbo: Aim, Shoot, Death, StayDead, StayAiming
