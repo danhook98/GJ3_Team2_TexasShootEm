14fe5cfbc45dd5733d7f918db1d27a71626ac1ef
Dan Hook <s188873@uos.ac.uk>
2025-03-09 16:02:14 +0000

    Add 'LevelLoadSO' checks to game manager

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace TexasShootEm
@@ -5,7 +6,38 @@ namespace TexasShootEm
     public class GameManager : MonoBehaviour
     {
         [SerializeField] private LevelLoadSO levelToLoad;
-        
-        
+
+        private bool _gameCanRun = true; 
+
+        private void Awake()
+        {
+            if (levelToLoad == null)
+            {
+                Debug.LogWarning("<color=red>Game Manager: </color>No level to load has been assigned.");
+                _gameCanRun = false;
+                return;
+            }
+
+            if (levelToLoad.loadedLevel == null)
+            {
+                Debug.LogWarning("<color=red>Game Manager: </color>A LevelLoadSO has been assigned, but it's " +
+                                 "'loadedLevel' value is null.");
+                _gameCanRun = false;
+                return; 
+            }
+        }
+
+        private void LoadLevel()
+        {
+            if (levelToLoad.loadedLevel.HasAccuracySlider)
+            {
+                
+            }
+
+            if (levelToLoad.loadedLevel.HasKeyPresses)
+            {
+                
+            }
+        }
     }
 }
