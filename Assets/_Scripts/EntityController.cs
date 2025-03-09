d0c97c52b793ea71203e6a341467bfa41818cddf
NikolaosComandariu <s283743@uos.ac.uk>
2025-03-09 12:19:00 +0000

    Fix animator again

--- Assets/_Scripts/EntityController.cs
+++ Assets/_Scripts/EntityController.cs
@@ -7,39 +7,42 @@ namespace TexasShootEm
     public class EntityController : MonoBehaviour
     {
         [Header("Events")] 
-        [SerializeField] private VoidEvent evilShoot;
-        [SerializeField] private VoidEvent evilDeath;
-        [SerializeField] private VoidEvent sheriffShoot;
-        [SerializeField] private VoidEvent sheriffDeath;
-        [SerializeField] private VoidEvent sheriffAim;
+        [SerializeField] private VoidEvent entityShoot;
+        [SerializeField] private VoidEvent entityDeath;
+        [SerializeField] private VoidEvent entityAim;
 
         [Header("Animators")] 
-        [SerializeField] private Animator sheriffAnim;
-        [SerializeField] private Animator evilAnim;
+        //[SerializeField] private Animator sheriffAnim;
+        [SerializeField] private Animator entityAnim;
         
         private bool _isDead = false;
+
+        private void Start()
+        {
+            entityAnim = GetComponent<Animator>();
+        }
 
         private void Update() // TODO: Remove this, this is here only for testing purposes.
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
                // StartCoroutine(EvilTubboShoot());
-                StartCoroutine(SheriffShoot());
+                StartCoroutine(EntityShoot());
             }
 
             if (Input.GetKeyDown(KeyCode.T))
             {
                 //StartCoroutine(EvilTubboDeath());
-                StartCoroutine(SheriffDeath());
+                StartCoroutine(EntityDeath());
             }
 
             if (Input.GetKeyDown(KeyCode.R))
             {
-                StartCoroutine(SheriffAim());
+                StartCoroutine(EntityAim());
             }
         }
 
-        public IEnumerator EvilTubboShoot()
+       /* public IEnumerator EvilTubboShoot()
         {
             if (!_isDead)
             {
@@ -59,40 +62,40 @@ namespace TexasShootEm
             evilDeath.Invoke(new Empty());
             yield return new WaitForSeconds(0.7f);
             evilAnim.SetTrigger("StayDead");
-        }
+        } */
 
-        public IEnumerator SheriffShoot()
+        public IEnumerator EntityShoot()
         {
             if (!_isDead) // Condition needed to avoid playing gunshot SFX when dead.
             {
-                Debug.Log("Starting player shoot coroutine");
-                sheriffAnim.SetTrigger("Shoot");
+                Debug.Log("Starting shoot coroutine");
+                entityAnim.SetTrigger("Shoot");
                 yield return new WaitForSeconds(0.2f);
-                sheriffShoot.Invoke(new Empty());
+                entityShoot.Invoke(new Empty());
                 yield return new WaitForSeconds(0.5f);
             }
         }
             
         
-        public IEnumerator SheriffDeath()
+        public IEnumerator EntityDeath()
         {
             _isDead = true;
-            Debug.Log("Starting player death coroutine");
-            sheriffAnim.SetTrigger("Death");
-            sheriffDeath.Invoke(new Empty());
+            Debug.Log("Starting death coroutine");
+            entityAnim.SetTrigger("Death");
+            entityDeath.Invoke(new Empty());
             yield return new WaitForSeconds(0.9f);
-            sheriffAnim.SetTrigger("StayDead");
+            entityAnim.SetTrigger("StayDead");
         }
 
-        public IEnumerator SheriffAim()
+        public IEnumerator EntityAim()
         {
             if (_isDead) yield return null;
             
-            Debug.Log("Starting player aim coroutine");
-            sheriffAnim.SetTrigger("Aim");
-            sheriffAim.Invoke(new Empty());
+            Debug.Log("Starting aim coroutine");
+            entityAnim.SetTrigger("Aim");
+            entityAim.Invoke(new Empty());
             yield return new WaitForSeconds(0.5f);
-            sheriffAnim.SetTrigger("StayAiming");
+            entityAnim.SetTrigger("StayAiming");
         }
     }
 }
