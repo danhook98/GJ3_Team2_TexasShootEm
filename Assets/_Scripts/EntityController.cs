d96eda12807f574a0be5dba4ca2027ed9f7f0ed8
NikolaosComandariu <s283743@uos.ac.uk>
2025-03-09 12:21:51 +0000

    Remove unnecessary code

--- Assets/_Scripts/EntityController.cs
+++ Assets/_Scripts/EntityController.cs
@@ -11,15 +11,12 @@ namespace TexasShootEm
         [SerializeField] private VoidEvent entityDeath;
         [SerializeField] private VoidEvent entityAim;
 
-        [Header("Animators")] 
-        //[SerializeField] private Animator sheriffAnim;
-        [SerializeField] private Animator entityAnim;
-        
+        private Animator _entityAnim;
         private bool _isDead = false;
 
         private void Start()
         {
-            entityAnim = GetComponent<Animator>();
+            _entityAnim = GetComponent<Animator>();
         }
 
         private void Update() // TODO: Remove this, this is here only for testing purposes.
@@ -42,49 +39,26 @@ namespace TexasShootEm
             }
         }
 
-       /* public IEnumerator EvilTubboShoot()
-        {
-            if (!_isDead)
-            {
-                Debug.Log("Starting enemy shoot coroutine");
-                evilAnim.SetTrigger("Shoot");
-                yield return new WaitForSeconds(0.5f);
-                evilShoot.Invoke(new Empty());
-                yield return new WaitForSeconds(0.5f);
-            }
-        }
-
-        public IEnumerator EvilTubboDeath()
-        {
-            _isDead = true;
-            Debug.Log("Starting enemy death coroutine");
-            evilAnim.SetTrigger("Death");
-            evilDeath.Invoke(new Empty());
-            yield return new WaitForSeconds(0.7f);
-            evilAnim.SetTrigger("StayDead");
-        } */
-
         public IEnumerator EntityShoot()
         {
             if (!_isDead) // Condition needed to avoid playing gunshot SFX when dead.
             {
                 Debug.Log("Starting shoot coroutine");
-                entityAnim.SetTrigger("Shoot");
+                _entityAnim.SetTrigger("Shoot");
                 yield return new WaitForSeconds(0.2f);
                 entityShoot.Invoke(new Empty());
                 yield return new WaitForSeconds(0.5f);
             }
         }
-            
         
         public IEnumerator EntityDeath()
         {
             _isDead = true;
             Debug.Log("Starting death coroutine");
-            entityAnim.SetTrigger("Death");
+            _entityAnim.SetTrigger("Death");
             entityDeath.Invoke(new Empty());
             yield return new WaitForSeconds(0.9f);
-            entityAnim.SetTrigger("StayDead");
+            _entityAnim.SetTrigger("StayDead");
         }
 
         public IEnumerator EntityAim()
@@ -92,10 +66,10 @@ namespace TexasShootEm
             if (_isDead) yield return null;
             
             Debug.Log("Starting aim coroutine");
-            entityAnim.SetTrigger("Aim");
+            _entityAnim.SetTrigger("Aim");
             entityAim.Invoke(new Empty());
             yield return new WaitForSeconds(0.5f);
-            entityAnim.SetTrigger("StayAiming");
+            _entityAnim.SetTrigger("StayAiming");
         }
     }
 }
