9dcfcdb67f7c43200b1b2027bfae4747146c9032
Dan Hook <s188873@uos.ac.uk>
2025-03-12 12:05:37 +0000

    Fix build compile errors
    The countdown script had a using directive for some Android input.

--- Assets/_Scripts/CountdownController.cs
+++ Assets/_Scripts/CountdownController.cs
@@ -1,10 +1,6 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
-using UnityEngine.InputSystem.Android.LowLevel;
-using UnityEngine.UI;
+using TMPro;
 
 namespace TexasShootEm
 {
